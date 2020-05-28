033ea889644961c543ae918aa9dbe2082e2572ee
Kyrah Wiggins <kyrah.wiggins@gmail.com>
2020-05-28 01:24:57 +0000

    Automatically backed up by Learn

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -1,11 +1,15 @@
 class SecretsController < ApplicationController
+
   before_action :require_login
+
   def show
+
   end
 
   private
 
-  def require_login
-    redirect_to '/login' unless session.include? :name
-  end
-end
+    def require_login
+      unless session.include? :name
+        redirect_to '/login'
+      end
+    end
